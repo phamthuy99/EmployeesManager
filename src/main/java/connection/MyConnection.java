014e85cfab53f2400e26a3466373b4e08f0af72e
phamthuy99 <pnthuy1999@gmail.com>
2023-03-23 08:15:22 +0700

    add new future

--- src/main/java/connection/MyConnection.java
+++ src/main/java/connection/MyConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 public final class MyConnection {
 
     private final static String DB_USER = "root";
-    private final static String DB_PASSWORD = "root";
+    private final static String DB_PASSWORD = "admin";
 
-    private final static String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/employeesmanager";
+    private final static String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/employees_manager";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_CONNECTION_URL, DB_USER, DB_PASSWORD);
---
+++ src/main/java/dao/DepartmentDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import connection.MyConnection;
+import model.Department;
+import model.Employee;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentDAO {
+
+
+
+}
